51b9a5be597e9f491388efa503bd583cbaa686b0
Maxim Samsonov <m.samsonov@computer.org>
2024-07-11 23:56:43 +0300

    Snapshot 1

--- dkgWebNode/Program.cs
+++ dkgWebNode/Program.cs
@@ -1,10 +1,14 @@
+using dkgWebNode.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+
 
 namespace dkgWebNode
 {
     public class Program
     {
+        private static DkgWebNodeService? DkgNodeService;
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -12,8 +16,22 @@ namespace dkgWebNode
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<DkgWebNodeService>();
 
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+            DkgNodeService = host.Services.GetRequiredService<DkgWebNodeService>();
+
+            await host.RunAsync();
+        }
+
+        [JSInvokable]
+        public static async Task SaveDataOnShutdown()
+        {
+            if (DkgNodeService is not null)
+            {
+                await DkgNodeService.SaveDataOnShutdown();
+            }
         }
     }
 }
+
---
+++ dkgWebNode/Services/DkgWebNodeService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.JSInterop;
+
+namespace dkgWebNode.Services
+{
+    public class DkgWebNodeService
+    {
+        private readonly IJSRuntime _jsRuntime;
+        private readonly string _keyStoreKey = "KeyStore";
+        private string? _keyStore = null;
+
+        public DkgWebNodeService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task Initialize()
+        {
+            await LogToConsoleAsync("Initializing DkgWebNodeService ...");
+            _keyStore = await GetValueFromLocalStorageAsync(_keyStoreKey);
+        }
+
+        public async Task LogToConsoleAsync(string message)
+        {
+            await _jsRuntime.InvokeVoidAsync("console.log", message);
+        }
+
+        private async Task<string?> GetValueFromLocalStorageAsync(string key)
+        {
+            return await _jsRuntime.InvokeAsync<string?>("getFromLocalStorage", key);
+        }
+
+        public async Task SaveDataOnShutdown()
+        {
+            if (_keyStore is not null)
+            {
+                await _jsRuntime.InvokeVoidAsync("saveToLocalStorage", _keyStoreKey, _keyStore);
+            }
+        }
+
+    }
+}
