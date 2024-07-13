64e52b6a063a54eff805ba2e036fdf8299dc9b10
Maxim Samsonov <m.samsonov@computer.org>
2024-07-14 01:09:25 +0300

    Fixed release tag information

--- dkgServiceNode/Data/DbEnsure.cs
+++ dkgServiceNode/Data/DbEnsure.cs
@@ -212,6 +212,7 @@ namespace dkgServiceNode.Data
             Ensure_0_8_0(connection);
             PuVersionUpdate("0.8.1", connection);
             PuVersionUpdate("0.8.2", connection);
+            PuVersionUpdate("0.9.1", connection);
         }
     }
 
